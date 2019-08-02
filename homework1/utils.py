e949799c48127a2afe5d662ab0ad6d7ef8a10ca4
Wangpeiyi9979 <wangpeiyi9979@gmail.com>
2019-08-02 11:43:29 +0800

    更改数据加载函数

--- homework1/utils.py
+++ homework1/utils.py
@@ -21,11 +21,8 @@ def data_load(path):
     """
     功能：使用pytorch装在cifar-10数据集
     输出:
-        train_data(float.tensor): (50000,3,32,32),
-        train_label(int.tensor): (50000),
-        test_data(float.tensor): (10000,3,32,32),
-        test_label(int.tensor): (10000)
-
+        train_set
+        test_set
     """
     dset.CIFAR10(path, train=True, transform=None, target_transform=None, download=True)
     dset.CIFAR10(path, train=False, transform=None, target_transform=None, download=True)
@@ -36,14 +33,26 @@ def data_load(path):
     train_set = tv.datasets.CIFAR10(root=path, train=True, download=True, transform=transform)
     test_set = tv.datasets.CIFAR10(root=path, train=False, download=True, transform=transform)
 
-    train_loader = torch.utils.data.DataLoader(train_set, batch_size=50000, shuffle=True, num_workers=0)
-    test_loader = torch.utils.data.DataLoader(test_set, batch_size=10000, shuffle=False, num_workers=0)
+    return train_set, test_set
 
-    train_data_interator = enumerate(train_loader)
-    test_data_interator = enumerate(test_loader)
+class RunningAverage():
+    """A simple class that maintains the running average of a quantity
+    Example:
+    ```
+    loss_avg = RunningAverage()
+    loss_avg.update(2)
+    loss_avg.update(4)
+    loss_avg() = 3
+    ```
+    """
 
-    train_data = next(train_data_interator)
-    test_data = next(test_data_interator)
+    def __init__(self):
+        self.steps = 0
+        self.total = 0
 
-    labels = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
-    return train_data[1][0], train_data[1][1], test_data[1][0], test_data[1][1], labels
+    def update(self, val):
+        self.total += val
+        self.steps += 1
+
+    def __call__(self):
+        return self.total / float(self.steps)
